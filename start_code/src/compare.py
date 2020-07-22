27384fedb3a540ad715740dab2d41c648799f502
Timothy Saudners <timsaunders88@gmail.com>
2020-07-22 17:09:59 +0100

    function works if num_1 == num_2

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -3,3 +3,5 @@ def compare(num_1, num_2):
         return f"{num_1} is greater than {num_2}"
     elif num_2 > num_1:
         return f"{num_1} is less than {num_2}"
+    else:
+        return "The two numbers are the same"
--- start_code/tests/compare_test.py
+++ start_code/tests/compare_test.py
@@ -9,3 +9,6 @@ class TestCompare(unittest.TestCase):
 
     def test_compare_1_3_returns_1_is_les_than_3(self):
         self.assertEqual("1 is less than 3", compare(1,3))
+    
+    def test_compare_3_3_returns_the_two_numbers_are_the_same(self):
+        self.assertEqual("The two numbers are the same", compare(3,3))
